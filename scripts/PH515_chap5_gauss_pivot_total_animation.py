f0d0338bb3c27fd92bdfa540647fa9d4990c2cec
NicOudart <nicolas.oudart.portable.45@gmail.com>
2025-04-27 19:33:52 +0200

    -Adding scripts for the animation of all Gauss methods for Chapter 5.

---
+++ scripts/PH515_chap5_gauss_pivot_total_animation.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+pos_rec = np.array([4205,158,4777],dtype=np.float64) #Coordonnées du récepteur GPS
+
+pos_sat1 = np.array([14000,4000,25000],dtype=np.float64) #Coordonnées du satellite GPS 1
+pos_sat2 = np.array([9000,-14000,21000],dtype=np.float64) #Coordonnées du satellite GPS 2
+pos_sat3 = np.array([24000,6000,15000],dtype=np.float64) #Coordonnées du satellite GPS 3
+pos_sat4 = np.array([10000,16000,19000],dtype=np.float64) #Coordonnées du satellite GPS 4
+
+#Génération des temps de retard------------------------------------------------
+
+t1 = ((sum((pos_rec-pos_sat1)**2))**0.5)/3e5 #Temps de retard associé au satellite GPS 1
+t2 = ((sum((pos_rec-pos_sat2)**2))**0.5)/3e5 #Temps de retard associé au satellite GPS 2
+t3 = ((sum((pos_rec-pos_sat3)**2))**0.5)/3e5 #Temps de retard associé au satellite GPS 3
+t4 = ((sum((pos_rec-pos_sat4)**2))**0.5)/3e5 #Temps de retard associé au satellite GPS 4
+
+
+#Génération des matrices A et b------------------------------------------------
+
+A = np.vstack((pos_sat2-pos_sat1,pos_sat3-pos_sat1,pos_sat4-pos_sat1))
+
+b_row1 = 0.5*(sum(pos_sat2**2)-sum(pos_sat1**2)-(3e5*t2)**2+(3e5*t1)**2)
+b_row2 = 0.5*(sum(pos_sat3**2)-sum(pos_sat1**2)-(3e5*t3)**2+(3e5*t1)**2)
+b_row3 = 0.5*(sum(pos_sat4**2)-sum(pos_sat1**2)-(3e5*t4)**2+(3e5*t1)**2)
+
+b = np.array([b_row1,b_row2,b_row3])
+
+#Gauss pivot total-------------------------------------------------------------
+    
+#Récupérer les dimensions de la matrice A :
+m,n = np.shape(A)
+
+#Créer un vecteur contenant l'ordre des inconnues dans x (de 0 à n-1) :
+idx_x = np.arange(n)
+
+print('Système initial :')
+print('A = '+str(A))
+print('b = '+str(b))
+print('x = '+str(idx_x))
+print('-----------')
+
+#Vérification des dimensions de A (nxn) et b (n) :
+if (m!=n)or(len(b)!=n):
+    
+    raise ValueError("Le système n'est pas de Cramer")
+ 
+#Copier A et b pour ne pas modifier les matrices originales :
+A_2 = np.copy(A)
+b_2 = np.copy(b)
+
+#Boucle sur les colonnes de la matrice A, jusqu'à l'avant-dernière :
+for j in range(n-1):
+    
+    print('Itération colonne '+str(j+1))
+    
+    #Sélection du pivot comme étant la valeur maximale en absolu sur la 
+    #portion de matrice non-triangularisée :
+    idx_pivot = np.argmax(abs(A_2[j:,j:])) #Indice du pivot
+    ligne_pivot = idx_pivot//(n-j)+j
+    colonne_pivot = idx_pivot%(n-j)+j
+    pivot = A_2[ligne_pivot,colonne_pivot] #Valeur du pivot
+    print('Pivot = '+str(pivot))
+    
+    #Si le pivot n'est pas sur la j-ième ligne, échanger la j-ième et la
+    #ligne du pivot :
+    if ligne_pivot!=j:
+        A_2[[j,ligne_pivot]] = A_2[[ligne_pivot,j]] #Pour la matrice A
+        b_2[[j,ligne_pivot]] = b_2[[ligne_pivot,j]] #Pour le vecteur b
+        print('Echange L'+str(ligne_pivot+1)+' et L'+str(j+1))
+        print('A = '+str(A_2))
+        print('b = '+str(b_2))
+        print('x = '+str(idx_x))
+        
+    #Si le pivot n'est pas sur la j-ième colonne, échanger la j-ième et la
+    #colonne du pivot :
+    if colonne_pivot!=j:
+        A_2[:,[j,colonne_pivot]] = A_2[:,[colonne_pivot,j]] #Pour la matrice A
+        idx_x[[j,colonne_pivot]] = idx_x[[colonne_pivot,j]] #Pour les éléments de x
+        print('Echange C'+str(colonne_pivot+1)+' et C'+str(j+1))
+        print('A = '+str(A_2))
+        print('b = '+str(b_2))
+        print('x = '+str(idx_x))
+        
+    #On vérifie que le pivot n'est pas nul :
+    if pivot!=0:
+        
+        #Boucle sur les lignes sous le pivot :
+        for k in range(j+1,n):
+            
+            #Opérations sur les lignes de A et b en utilisant le pivot :
+            print('Opération L'+str(k+1)+' = L'+str(k+1)+' - L'+str(j+1)+'x'+str(A_2[k,j])+'/'+str(pivot))
+            b_2[k] = b_2[k] - b_2[j]*A_2[k,j]/pivot
+            A_2[k,:] = A_2[k,:] - A_2[j,:]*A_2[k,j]/pivot
+            
+    print('A = '+str(A_2))
+    print('b = '+str(b_2))
+    print('-----------')
